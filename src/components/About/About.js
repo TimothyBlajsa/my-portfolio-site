0ad24aee03347c066c78bff792626661bd6731d9
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-17 15:03:25 -0400

    Link placeholder image
    Create image folder

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,12 +2,14 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
+import image from '../../images/logo192.png'
 
 const About = () => {
   const { name, role, description, resume, social } = about
 
   return (
     <div className='about center'>
+      <img src={image} alt='profile-pic' />
       {name && (
         <h1>
           Hi, I am <span className='about__name'>{name}.</span>
