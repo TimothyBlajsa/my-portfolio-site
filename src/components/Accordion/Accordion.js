f69cda357ed1ca6b21bb57ad9f0d90ea40283a00
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-17 22:18:28 -0400

    Create AccordionDrawer component

---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetils from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpanMore';
+
+const AccordionDrawer = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default AccordionDrawer;
