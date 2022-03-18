5cb4bdd992cba6f5349b64d7d37854787cec0a12
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-18 15:42:26 -0400

    Remove unused imports

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,19 +1,25 @@
-import AwesomeSlider from 'react-awesome-slider';
 import '../Gallery/Gallery.css';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import image from '../../images/moon.png'
+import crow from '../../images/crow.jpeg';
+import drown from '../../images/drown.jpeg';
+import vulture from '../../images/vulture.jpeg';
 
-console.log({image});
+
 
 const Gallery = () => (
     <>
     <section id='gallery' className='section gallery'>
         <h2 className='section__title'>Gallery</h2>
+        
         <div className='project'>
-            <img src={image} alt='lunar' className='gallery__image' /> 
+            <img src={crow} alt='bird_drawn' className='gallery__image' />
+        </div>
+
+        <div className='project'>
+            <img src={drown} alt='anime_drawn' className='gallery__image' />
+        </div>
+
+        <div className='project'>
+            <img src={vulture} alt='prey' className='gallery__image' />
         </div>
     </section>
     </>
