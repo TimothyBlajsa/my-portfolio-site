3d819a05b6c989ca52329e147043370400dcc413
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-19 13:00:58 -0400

    Update About links to new tab

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,7 +21,7 @@ const About = () => {
 
       <div className='about__contact center'>
         {resume && (
-          <a href={resume}>
+          <a href={resume} target='_blank' rel='noreferrer'>
             <span type='button' className='btn btn--outline'>
               Resume
             </span>
@@ -35,6 +35,8 @@ const About = () => {
                 href={social.github}
                 aria-label='github'
                 className='link link--icon'
+                target='_blank'
+                rel='noreferrer'
               >
                 <GitHubIcon />
               </a>
@@ -45,6 +47,8 @@ const About = () => {
                 href={social.linkedin}
                 aria-label='linkedin'
                 className='link link--icon'
+                target='_blank'
+                rel='noreferrer'
               >
                 <LinkedInIcon />
               </a>
