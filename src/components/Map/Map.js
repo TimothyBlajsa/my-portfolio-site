3fe866996ce7f1aaa25ea951900ac49ad1a87214
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-19 22:41:50 -0400

    Convert coordinate variables into object

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,15 @@
+import '../Map/Map.css'
+
+const width  = '100%';
+const height = '400';
+const coords = {
+    lat: '40.67139',
+    lon: '-74.42997',
+}
+
 const Location = () => {
     return (
-        <iframe width="100%" height="300" src="https://api.maptiler.com/maps/toner/?key=gfuXlNxZzutozeCvcRzR#10.6/40.67139/-74.42997" title='BH__map'></iframe>
+        <iframe width={width} height={height} src={`https://api.maptiler.com/maps/toner/?key=gfuXlNxZzutozeCvcRzR#10.6/${coords.lat}/${coords.lon}`} title='BH__map'></iframe>
     )
 }
 
