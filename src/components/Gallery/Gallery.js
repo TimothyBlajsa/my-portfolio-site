3c2e4662ba6ad5129389b6eb19514fe295e9d66e
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-04-13 18:02:12 -0400

    Add manifest.json file

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,11 +2,15 @@ import '../Gallery/Gallery.css';
 import { IMAGES } from '../../portfolio';
 import DztImageGalleryComponent from 'reactjs-image-gallery';
 
+let data;
+
+console.log(document.getElementsByClassName('styles_wrapper_1kG3C'));
+
 const Gallery = () => (
     <>
     <h2 className='section__title'>Gallery</h2>
     <section id='gallery' className='section gallery'>
-        <DztImageGalleryComponent images={IMAGES} style={{backgroundColor: 'none'}}/>
+        <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='none'/>
     </section>
     </>
     
