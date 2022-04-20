026da6fca0bd02c9cd38e3d7a7c66e60434efd0c
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-04-20 18:07:24 -0400

    Fix manifest.json file

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,13 +4,15 @@ import DztImageGalleryComponent from 'reactjs-image-gallery';
 
 let data;
 
-console.log(document.getElementsByClassName('styles_wrapper_1kG3C'));
+//console.log(document.getElementsByClassName('styles_wrapper_1kG3C'));
+//console.log(document.getElementsByName('figcaption'))
+//document.getElementsByClassName('styles_wrapper_1kG3C').styles.backgroundColor = 'none';
 
 const Gallery = () => (
     <>
     <h2 className='section__title'>Gallery</h2>
     <section id='gallery' className='section gallery'>
-        <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='none'/>
+        <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='none' id='gal-id'/>
     </section>
     </>
     
