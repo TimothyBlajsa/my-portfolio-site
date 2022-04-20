dec63ca421f9582dfd48d7de65be2b403e08c59d
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-04-20 18:29:31 -0400

    Resolve gallery background and min-height

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -6,13 +6,13 @@ let data;
 
 //console.log(document.getElementsByClassName('styles_wrapper_1kG3C'));
 //console.log(document.getElementsByName('figcaption'))
-//document.getElementsByClassName('styles_wrapper_1kG3C').styles.backgroundColor = 'none';
+document.getElementsByClassName('styles_wrapper_1kG3C').backgroundColor = 'none';
 
 const Gallery = () => (
     <>
     <h2 className='section__title'>Gallery</h2>
     <section id='gallery' className='section gallery'>
-        <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='none' id='gal-id'/>
+        <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='white' id='gal-id' background-color='none'/>
     </section>
     </>
     
