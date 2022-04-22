01afba8c638162d2eab91d6852376081242f0267
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-04-22 16:27:07 -0400

    Remove unused attributes

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,15 +4,11 @@ import DztImageGalleryComponent from 'reactjs-image-gallery';
 
 let data;
 
-//console.log(document.getElementsByClassName('styles_wrapper_1kG3C'));
-//console.log(document.getElementsByName('figcaption'))
-document.getElementsByClassName('styles_wrapper_1kG3C').backgroundColor = 'none';
-
 const Gallery = () => (
     <>
     <section id='gallery' className='section gallery'>
     <h2 className='section__title'>Gallery</h2>
-        <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='white' id='gal-id' background-color='none'/>
+        <DztImageGalleryComponent images={IMAGES} data-gal={data} />
     </section>
     </>
     
