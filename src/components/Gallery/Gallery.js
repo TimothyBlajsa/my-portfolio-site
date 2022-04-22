630f2ea516ef99f2133a4a45cd7ad66f49a2fd0f
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-04-22 16:25:53 -0400

    Correct section link to Gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -10,8 +10,8 @@ document.getElementsByClassName('styles_wrapper_1kG3C').backgroundColor = 'none'
 
 const Gallery = () => (
     <>
-    <h2 className='section__title'>Gallery</h2>
     <section id='gallery' className='section gallery'>
+    <h2 className='section__title'>Gallery</h2>
         <DztImageGalleryComponent images={IMAGES} data-gal={data} imageBackgroundColor='white' id='gal-id' background-color='none'/>
     </section>
     </>
